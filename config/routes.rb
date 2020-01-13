dd61f8bcd6b8a820abd25a52e0f92164b24e94fe
Kevin Osborn <kevinosborn31@gmail.com>
2020-01-13 18:31:35 +1100

    Created routing for individual restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'restaurants/index'
   get 'restaurants/show'
+  get '/restaurants/show/:id' => 'restaurants#show'
   get 'restaurants/edit'
   get 'restaurants/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
