7d6aad6147359176443a349d96585ffc8b954c0d
Kevin Osborn <kevinosborn31@gmail.com>
2020-01-14 12:57:01 +1100

    Updated Routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,21 +2,53 @@ class RestaurantsController < ApplicationController
 
   skip_before_action :verify_authenticity_token, raise: false
 
+  def new
+  end
+
+  def create
+    Restaurant.create(
+      name: params[:name],
+      price: params[:price],
+      cuisine: params[:cuisine],
+      location: params[:location],
+      photo: params[:photo]
+    )
+
+    redirect_to( restaurants_path )
+  end
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find( params[:id] )
+    @restaurant = Restaurant.find params[:id]
   end
 
   def edit
-    @restaurant = Restaurant.find( params[:id] )
+    @restaurant = Restaurant.find params[:id]
   end
 
-  def new
-  end
+  def update
+    restaurant = Restaurant.find params[:id]
+    restaurant.update(
+      name: params[:name],
+      price: params[:price],
+      cuisine: params[:cuisine],
+      location: params[:location],
+      photo: params[:photo]
+    )
 
-  def create
-  end
+    redirect_to restaurant_path(restaurant.id)
+
+
+
+end
+
+def destroy
+  Restaurant.destroy params[:id]
+
+  redirect_to( planets_path )
+end
+
 end
