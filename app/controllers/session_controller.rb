7470af4704d06e9fc24411d9b7f04315b5e11b0d
Kevin Osborn <kevinosborn31@gmail.com>
2020-01-15 12:09:10 +1100

    Updated session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,18 +4,23 @@ class SessionController < ApplicationController
   end
 
   def create
-    user = User.find_bby :email => params[:email]
-    if user.present? && user.authenticate(params[:password])
+    user = User.find_by email: params[:email]
+
+    if user.present? && user.authenticate( params[:password] )
+      # successful login
+
       session[:user_id] = user.id
-      redirect_to root_path
+
+      redirect_to user_path(user.id) # go to show page (/users/10 etc)
+
     else
-      flash[:error] = "User could not be created!"
-      redirect_to login_path
-    end
+    flash[:error] = 'Invalid email or password'
+    redirect_to login_path #show form again
+  end
 end
 
 def destroy
-  session[:user_id] = nil
+  session[:user_id] = nil # logs out the user
   redirect_to login_path
 end
 end
